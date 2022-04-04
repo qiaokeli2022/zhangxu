d81629848564dc718380adde588b877f46ac0892
emothin <101618121+emothin@users.noreply.github.com>
2022-04-04 16:53:07 +0800

    Add files via upload

---
+++ test/019tom.java
@@ -0,0 +1,16 @@
+public  class  tom{
+	public static void  main (String[] args){
+		System.out.println("hello,world~");
+	}
+}
+
+class  Dog{
+	public static void  main (String[] args){
+		System.out.println("hello,xiaogougou");
+	}
+}
+ class  Tiger{
+	public static void  main (String[] args){
+		System.out.println("hello,xiaolaohu");
+	}
+}
