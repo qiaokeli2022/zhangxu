9ee630a3d44a66521e29c97c520f5520be851309
emothin <101618121+emothin@users.noreply.github.com>
2022-04-05 15:39:45 +0800

    Add files via upload

---
+++ test/021.java
@@ -0,0 +1,12 @@
+public  class  ChangeChar{
+	//编写一个main方法
+	public  static  void main (String[] args){
+//    \t :一个制表位，实现对齐功能
+	System.out.println("北京\t天津\t上海");
+//  \n:换行符
+// \\:一个\
+// \" ：一个"
+// \'：一个'
+// \r  :一个回车 System.out.println("学习\r北京")；
+	}
+}
